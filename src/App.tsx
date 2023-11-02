fcf6644003fc8371d070177be957ae87d98c145f
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2023-11-02 11:00:57 +0100

    json server

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import GeneralStyles from './shared/styles/GeneralStyles'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -8,6 +9,22 @@ import Header from './layout/Header'
 import Main from './layout/Main'
 
 function App() {
+    const handleError = async (error: string) => {
+        await console.log(error)
+    }
+
+    const fetchData = () => {
+        fetch('http://localhost:4000/data')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+            })
+            .catch((error) => handleError(error))
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
         <div id="app-wrapper">
             <GeneralStyles />
