ac63dc5150b2a4b8e9aeaee620077c06e07e230e
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2023-12-21 16:00:24 +0100

    header reduce rerender

--- src/components/Header/ControlsModalBtn/index.tsx
+++ src/components/Header/ControlsModalBtn/index.tsx
@@ -1,11 +1,34 @@
+import { useState } from 'react'
+
+import Modal from '../../../components/Reusable/Modal'
+import ControlsModal from '../../../components/Modals/ControlsModal'
+
 import { BtnWrap } from './style'
 
-interface Props {
-    onClickBtn: React.MouseEventHandler<HTMLButtonElement>
-}
+const ControlsModalBtn = () => {
+    const [openModal, setOpenModal] = useState<boolean>(false)
 
-const ControlsModalBtn = ({ onClickBtn }: Props) => {
-    return <BtnWrap onClick={onClickBtn}>Controls Modal</BtnWrap>
+    //On click btn in left corner of header, modal opens
+    const onClickHandler = () => {
+        //Opening modal
+        setOpenModal(true)
+    }
+
+    //On click modal overlay or x btn, modal closes
+    const onCloseHandler = () => {
+        setOpenModal(false)
+    }
+
+    return (
+        <>
+            <BtnWrap onClick={onClickHandler}>Controls Modal</BtnWrap>
+            {openModal && (
+                <Modal onClose={onCloseHandler} overlayDisplay={true}>
+                    <ControlsModal closeModal={onCloseHandler} />
+                </Modal>
+            )}
+        </>
+    )
 }
 
 export default ControlsModalBtn
--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,41 +1,19 @@
-import { useState } from 'react'
 import ControlsModalBtn from '../../components/Header/ControlsModalBtn'
 import Balance from '../../components/Header/Balance'
 import WalletConnect from '../../components/Header/WalletConnect'
 import Notifications from '../../components/Header/Notifications'
 
-import Modal from '../../components/Reusable/Modal'
-import ControlsModal from '../../components/Modals/ControlsModal'
-
 import { HeaderWrap, HeaderInner } from './style'
 
 const Header = () => {
-    const [openModal, setOpenModal] = useState<boolean>(false)
-
-    //On click btn in left corner of header, modal opens
-    const onClickHandler = () => {
-        //Opening modal
-        setOpenModal(true)
-    }
-
-    //On click modal overlay or x btn, modal closes
-    const onCloseHandler = () => {
-        setOpenModal(false)
-    }
-
     return (
         <HeaderWrap>
-            <ControlsModalBtn onClickBtn={onClickHandler} />
+            <ControlsModalBtn />
             <HeaderInner>
                 <Balance />
                 <WalletConnect />
                 <Notifications />
             </HeaderInner>
-            {openModal && (
-                <Modal onClose={onCloseHandler} overlayDisplay={true}>
-                    <ControlsModal closeModal={onCloseHandler} />
-                </Modal>
-            )}
         </HeaderWrap>
     )
 }
